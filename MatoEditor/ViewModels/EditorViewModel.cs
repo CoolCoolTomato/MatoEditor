222a0c9f95f302f3066ddbeb8a8d9fcc8d655dfd
CoolCoolTomato <2983315455@qq.com>
2024-08-17 16:55:54 +0800

    添加视图切换功能

--- MatoEditor/ViewModels/EditorViewModel.cs
+++ MatoEditor/ViewModels/EditorViewModel.cs
@@ -21,6 +21,11 @@ public class EditorViewModel : ViewModelBase
         
         ContentString = "";
         ContentHtml = "";
+
+        EditorVisible = true;
+        ViewerVisible = true;
+        SetEditorModeCommand = ReactiveCommand.Create<string>(SetEditorMode);
+        
         _storageService.WhenAnyValue(x => x.CurrentFilePath)
             .Subscribe(CurrentFilePath =>
             {
@@ -73,5 +78,38 @@ public class EditorViewModel : ViewModelBase
     private async void SaveFile()
     {
         _ = await _fileSystemService.WriteFileAsync(_storageService.CurrentFilePath, ContentString);
+    }
+
+    private bool _editorVisible;
+    public bool EditorVisible
+    {
+        get => _editorVisible;
+        set => this.RaiseAndSetIfChanged(ref _editorVisible, value);
+    }
+    private bool _viewerVisible;
+    public bool ViewerVisible
+    {
+        get => _viewerVisible;
+        set => this.RaiseAndSetIfChanged(ref _viewerVisible, value);
+    }
+    
+    public ICommand SetEditorModeCommand { get; }
+    private void SetEditorMode(string mode)
+    {
+        if (mode == "edit")
+        {
+            EditorVisible = true;
+            ViewerVisible = false;
+        }
+        else if (mode == "view")
+        {
+            EditorVisible = false;
+            ViewerVisible = true;
+        }
+        else
+        {
+            EditorVisible = true;
+            ViewerVisible = true;
+        }
     }
 }
