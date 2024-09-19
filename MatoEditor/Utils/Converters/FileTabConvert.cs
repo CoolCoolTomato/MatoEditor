a829c37ce62d716920ec3bb1ce2ed83c24ef8db4
CoolCoolTomato <2983315455@qq.com>
2024-09-19 22:53:49 +0800

    添加文件未保存状态

---
+++ MatoEditor/Utils/Converters/FileTabConvert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+
+namespace MatoEditor.Utils.Converters;
+
+public class FileTabConverter : IMultiValueConverter
+{
+    public object Convert(IList<object?> values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values[0] is string oldContent && values[1] is string newContent)
+        {
+            return oldContent != newContent;
+        }
+        return AvaloniaProperty.UnsetValue;
+    }
+}
